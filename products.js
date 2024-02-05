d7cb5cc4f80f1d9a6cbab62169606b2ae749a2d1
Alycia <qw159321@gmail.com>
2024-02-05 22:11:34 +0800

    adjust window.location

--- products.js
+++ products.js
@@ -20,7 +20,7 @@ const app = createApp({
         })
         .catch((err) => {
           alert(err.response.data.message);
-          window.location = 'login.html';
+          window.location = 'index.html';
         });
     };
 
